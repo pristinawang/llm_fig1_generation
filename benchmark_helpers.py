985309eff85b7c40b751ea05967d5a073be92b96
pwang71 <pwang71@login02.cm.cluster>
2025-03-26 18:59:50 -0400

    added boiler plates

---
+++ benchmark_helpers.py
@@ -0,0 +1,14 @@
+def return_fig1_abstract(tex_file_path='/home/2401.07159v1/acl_latex.tex'):
+    '''
+    tex_file_path: absolute file path (str)
+    fig1_file_path: absolute file path(str)
+    abstract_str: string
+    fig1_caption_str: string 
+    '''
+    
+    # if fig1 exists in this file:
+    #     return fig1_file_path, abstract_str, fig1_caption_str
+    # else:
+    #     return None 
+    
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from benchmark_helpers import *
+def main():
+    # Use benchmark_helpers to crawl bench mark
+    pass
+
+if __name__ == "__main__":
+    main()
