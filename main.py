181269b39560faa969c8ca026aed47f397d036a3
pwang71 <pwang71@login02.cm.cluster>
2025-03-26 20:24:13 -0400

    all features besides return_fig1_abstract done

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from benchmark_helpers import *
+
 def main():
     # Use benchmark_helpers to crawl bench mark
-    pass
+    year='2024'
+    titles=return_acl_paper_titles(year=year)
+    titles_small=titles[:10]
+    paper_id_dict, not_found_titles = get_arxiv_id_dict(titles=titles_small, max_results=10)
+    download_latex_files(paper_id_dict=paper_id_dict, save_dir_path='./arxiv_source/')
+    extract_to_csv(paper_id_dict=paper_id_dict, latex_files_path='./arxiv_source', csv_path='./benchmark/fig1_bench.csv')
 
 if __name__ == "__main__":
     main()
